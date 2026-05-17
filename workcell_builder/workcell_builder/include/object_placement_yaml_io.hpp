9a83955fd55bd4da89d7fac12ac7c344c1a9e7f6
notanexpert <121873648+Mukaram01@users.noreply.github.com>
2026-05-17 18:45:06 +0100

    Fix TaskZone/model linkage and test deps in workcell_builder

--- workcell_builder/workcell_builder/include/object_placement_yaml_io.hpp
+++ workcell_builder/workcell_builder/include/object_placement_yaml_io.hpp
@@ -16,17 +16,6 @@ struct PlacedObjectYamlWriteResult
   size_t objects_saved{0};
 };
 
-struct TaskZone
-{
-  std::string id;
-  std::string type{"pick"};
-  double x{0.0}, y{0.0}, z{0.0};
-  double roll{0.0}, pitch{0.0}, yaw{0.0};
-  double size_x{0.2}, size_y{0.2}, size_z{0.2};
-  std::string frame{"world"};
-  std::string status;
-};
-
 std::vector<PlacedObject> load_placed_objects_from_environment_yaml(
   const std::string & environment_yaml_path,
   std::vector<std::string> * warnings = nullptr);
