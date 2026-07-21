91d1257947643ebfc35d523f609c0ec47d606dc2
notanexpert <121873648+Mukaram01@users.noreply.github.com>
2026-07-21 16:16:53 +0100

    Bootstrap curated add-object controller

---
+++ workcell_builder/workcell_builder/gui/embedded_web_curated_add_bootstrap.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm>
+#include "embedded_web_curated_add_controller.hpp"
+
+#ifdef WORKCELL_BUILDER_HAS_WEBENGINE
+
+#include <QApplication>
+#include <QTimer>
+
+namespace workcell_builder
+{
+inline void bootstrapEmbeddedWebCuratedAddControllers()
+{
+  if (!qApp || qApp->property("workcell_embedded_curated_add_bootstrap").toBool()) return;
+  qApp->setProperty("workcell_embedded_curated_add_bootstrap", true);
+  auto * timer = new QTimer(qApp);
+  timer->setInterval(500);
+  QObject::connect(timer, &QTimer::timeout, qApp, []() {
+    for (QWidget * window : QApplication::topLevelWidgets()) {
+      installEmbeddedWebCuratedAddControllers(window);
+    }
+  });
+  timer->start();
+  QTimer::singleShot(0, qApp, []() {
+    for (QWidget * window : QApplication::topLevelWidgets()) {
+      installEmbeddedWebCuratedAddControllers(window);
+    }
+  });
+}
+}  // namespace workcell_builder
+
+Q_COREAPP_STARTUP_FUNCTION(workcell_builder::bootstrapEmbeddedWebCuratedAddControllers)
+
+#endif
