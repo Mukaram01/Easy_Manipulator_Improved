7988b6e06eb3acf572800f2cffd454b66839dd68
notanexpert <121873648+Mukaram01@users.noreply.github.com>
2026-08-19 11:24:28 +0100

    Bootstrap crash-safe Home v5 layer

--- workcell_builder/workcell_builder/gui/startup_dialog.cpp
+++ workcell_builder/workcell_builder/gui/startup_dialog.cpp
@@ -1,6 +1,6 @@
 #include "gui/startup_dialog.h"
 #include "include/workspace_validation.hpp"
-#include "workcell_home_polish_v4.hpp"
+#include "workcell_home_polish_v5.hpp"
 
 #include <QComboBox>
 #include <QDir>
